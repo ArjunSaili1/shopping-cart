8455015fe7ed17ea70071cde31bb94c415d39feb
Arjun Saili <arjun7.saili@gmail.com>
2022-03-23 22:26:47 -0400

    set grid to recreate on every render

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -55,7 +55,7 @@ function MovieGrid(props){
       searchMovie(props.searchTerm).then((data)=>{
           setMovieCards(data)
       })
-    }, [])
+    })
 
     return(
         <div className="movie-grid">
