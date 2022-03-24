2461388936d4824b8e8d7ba6c1920885a0444799
Arjun Saili <arjun7.saili@gmail.com>
2022-03-24 18:26:00 -0400

    Add random price gen, movie img & add btn styles

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+
 function MovieCard(props){
+
+    const [price, setPrice] = useState(0);
+
+    const generateRandomPrice = () =>{
+        setPrice(Math.floor(Math.random() * (11) + 9) + 0.99)
+    }
+
+    useEffect(()=>{
+        generateRandomPrice();
+    }, [])
+
     return(
         <div className="movie-card">
             <div className="movie-details">
                 <img className="movie-img" alt={props.title} src={props.image}></img>
                 <h3>{props.title}</h3>  
+                <h3>{price}</h3>
                 <h4>{props.releaseDate}</h4>
                 <h5>{props.rating}/10</h5>
             </div>
