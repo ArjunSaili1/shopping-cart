be554d67d24aee2726aaae96fd296314e3b97619
Arjun Saili <arjun7.saili@gmail.com>
2022-03-24 18:03:29 -0400

    create add to cart btn & add styles

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 function MovieCard(props){
     return(
         <div className="movie-card">
-            <img alt={props.title} src={props.image}></img>
-            <h3>{props.title}</h3>  
-            <h4>{props.releaseDate}</h4>
-            <h5>{props.rating}/10</h5>
+            <div className="movie-details">
+                <img className="movie-img" alt={props.title} src={props.image}></img>
+                <h3>{props.title}</h3>  
+                <h4>{props.releaseDate}</h4>
+                <h5>{props.rating}/10</h5>
+            </div>
+            <div className="add-btn-container">
+                <button className="add-btn">Add to Cart</button>
+            </div>
         </div>
     )
 }
