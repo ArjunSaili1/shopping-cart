e865d946c1e2d0dec051a1e21988bf674e90a9d4
Arjun Saili <arjun7.saili@gmail.com>
2022-03-23 22:13:00 -0400

    Create search component with basic styling

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,15 +1,19 @@
 import MovieGrid from "./MovieGrid"
+import Searchbar from "./Searchbar"
 import { useEffect, useState } from "react"
 function MoviesPage(){
     
     const [movieGridComp, setMovieGridComp] = useState([])
 
     useEffect(()=>{
-        setMovieGridComp(<MovieGrid searchTerm="Frozen"/>)
+        setMovieGridComp(<MovieGrid searchTerm="Batman"/>)
     }, [])
 
     return(
         <div className="movies-page">
+            <div className="search-container">
+                <Searchbar/>    
+            </div>
             {movieGridComp}
         </div>
     )
---
+++ src/components/Searchbar.js
@@ -0,0 +1,7 @@
+function Searchbar(props){
+    return(
+        <input className="search-bar" type="text"></input>
+    )
+}
+
+export default Searchbar
