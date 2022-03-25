bd672ec4fa852ad5d9a4e42341b5677a359bb6bb
Arjun Saili <arjun7.saili@gmail.com>
2022-03-24 23:55:56 -0400

    Add default value to search bar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,7 +1,7 @@
 function Searchbar(props){
     return(
         <form onSubmit={props.search}>
-            <input className="search-bar" type="text"></input>
+            <input defaultValue="Batman" className="search-bar" type="text"></input>
             <button className='search-submit' type="submit">Search</button>
         </form>
     )
