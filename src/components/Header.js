1b4b1a2099cf5b1f9225b112d2449bacf9360e48
Arjun Saili <arjun7.saili@gmail.com>
2022-03-24 22:01:44 -0400

    Add shopping cart item count, add styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,16 @@ function Header(props){
             <nav className="header-nav">
                 <span className="logo-container">
                     <LogoSVG className="logo"/>
-                    <a className="title">Reel'd</a>
+                    <a href="/"className="title">Reel'd</a>
                 </span>
                 <a href="/"className="nav-links">Home</a>
                 <a href="/shop" className="nav-links">Shop</a>
-                <span className="cart-container">
+                <a href="/shop"className="cart-container">
                     <CartSVG className="cart-icon"/>
-                </span>
+                    <span className="cart-count-container">
+                        <span className="cart-count">{props.cart.length}</span>
+                    </span>
+                </a>
             </nav>
         </header>
     )
