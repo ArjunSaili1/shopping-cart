df87ef55184f1c5f8161495c7f63ceff3d4add6e
Arjun Saili <arjun7.saili@gmail.com>
2022-03-25 20:04:08 -0400

    Calculate and display total price

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,12 +1,22 @@
 import CartItem from "./CartItem"
 
 function ShoppingCart(props){
+
+    let totalPrice = 0;
+
     return(<div className="shopping-cart-sidebar">
         <div className="screen-overlay" onClick={props.toggleShoppingCart}></div>
         <div className="shopping-cart-view">
-            {props.cart.map(movie => {
-                return (<CartItem showAdd={movie["showAdd"]} removeItemFromCart={props.removeItemFromCart} key={movie["title"]} movie={movie}/>)
-            })}
+            <div className="product-view">
+                {props.cart.map(movie => {
+                    totalPrice += parseFloat(movie["price"].slice(1))
+                    return (<CartItem showAdd={movie["showAdd"]} 
+                    removeItemFromCart={props.removeItemFromCart} key={movie["title"]} movie={movie}/>)
+                })}
+            </div>
+            <div className="total-price">
+                <h2>{"Total: $".concat(Math.round(totalPrice * 100)/ 100)}</h2>
+            </div>
         </div>
     </div>)
 }
