d51709479df7e3c6d03d78c30e117abda692651c
Arjun Saili <arjun7.saili@gmail.com>
2022-03-27 01:28:57 -0400

    Update search form styling

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,6 +1,6 @@
 function Searchbar(props){
     return(
-        <form onSubmit={props.search}>
+        <form className="search-form" onSubmit={props.search}>
             <input defaultValue="Batman" className="search-bar" type="text"></input>
             <button className='search-submit' type="submit">Search</button>
         </form>
