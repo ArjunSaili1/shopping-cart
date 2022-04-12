ee6da1df9e2f2b4c96c5b1873e82676ca762190a
Arjun Saili <arjun7.saili@gmail.com>
2022-04-12 16:20:50 -0400

    Animate homepage to fade in on page switch

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,24 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 import '../styles/Homepage.css'
 
 function Homepage(){
     return(
-        <div className="homepage">
-            <div className="front-display">
-                <div className="front-text-ctn">
-                    <h1 className="front-text">Reel Movies, Reely Great.</h1>
-                    <h2 className="front-text-description">Find your next favourite movie <a className="here-text" href="/shop">here</a></h2>
+        <AnimatePresence>
+            <motion.div 
+            key="homepage"
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            className="homepage">
+                <div className="front-display">
+                    <div className="front-text-ctn">
+                        <h1 className="front-text">Reel Movies, Reely Great.</h1>
+                        <h2 className="front-text-description">Find your next favourite movie <Link className="here-text" to="/shop">here</Link></h2>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
