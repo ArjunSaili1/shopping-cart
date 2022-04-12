95abecbe688f01ccf7e9b64bcfadb9066b9be473
Arjun Saili <arjun7.saili@gmail.com>
2022-04-12 16:21:45 -0400

    animate movies to fade in/out when added/removed

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,8 +1,9 @@
 import CartItem from "./CartItem"
-import { AnimatePresence, motion } from "framer-motion";
 import { useContext } from "react"; 
 import CartContext from "../context/CartContext";
+import uniqid from 'uniqid'
 import '../styles/ShoppingCart.css'
+import { AnimatePresence, motion } from "framer-motion";
 
 function ShoppingCart({toggleShoppingCart}){
 
@@ -10,32 +11,49 @@ function ShoppingCart({toggleShoppingCart}){
 
     let totalPrice = 0;
 
-    return(<AnimatePresence>
-        <motion.div 
-            initial={{x: 300}}
-            animate={{x: 0}}
-            exit={{x: 300}} className="shopping-cart-sidebar">
-            <div 
-            className="screen-overlay" 
-            onClick={toggleShoppingCart}></div>
-            <div className="shopping-cart-view">
-                <div className="product-view">
-                    {cart.map(movie => {
-                        totalPrice += parseFloat(movie["price"].slice(1))
-                        return (<CartItem showAdd={movie["showAdd"]} 
-                        key={movie["title"]} movie={movie}/>)
-                    })}
-                </div>
-                <div className="shopping-cart-bottom">
-                    <div className="total-price">
-                        <h2>{"Total: $".concat(Math.round(totalPrice * 100)/ 100)}</h2>
+    return(
+    <>
+        <div className="screen-overlay" onClick={toggleShoppingCart}></div>
+        <AnimatePresence>
+            <motion.div
+            className="shopping-cart-view"
+            initial={{ opacity: 0.5 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0.5 }}
+            key={uniqid()}
+            >
+            <div className="product-view">
+                {cart.map((movie) => {
+                totalPrice += parseFloat(movie["price"].slice(1));
+                return (
+                    <div className="cart-item">
+                    <CartItem
+                        showAdd={movie["showAdd"]}
+                        key={movie["title"]}
+                        movie={movie}
+                    />
                     </div>
-                    <button className="shopping-cart-btn" onClick={()=>{alert("Thanks for visiting! :)")}}><h2>Checkout</h2></button>
-                    <button className="shopping-cart-btn" onClick={toggleShoppingCart}><h2>Close</h2></button> 
-                </div>
+                );
+                })}
             </div>
-        </motion.div>
-    </AnimatePresence>
-    )
+            <div className="shopping-cart-bottom">
+                <div className="total-price">
+                <h2>{"Total: $".concat(Math.round(totalPrice * 100) / 100)}</h2>
+                </div>
+                <button
+                className="shopping-cart-btn"
+                onClick={() => {
+                    alert("Thanks for visiting! :)");
+                }}
+                >
+                <h2>Checkout</h2>
+                </button>
+                <button className="shopping-cart-btn" onClick={toggleShoppingCart}>
+                <h2>Close</h2>
+                </button>
+            </div>
+            </motion.div>
+        </AnimatePresence>
+    </>)
 }
 export default ShoppingCart
